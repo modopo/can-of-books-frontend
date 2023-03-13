02650f8e827b2aa7979e484a446f9a52923ecccd
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-03-13 15:49:21 -0700

    Adjust stylings for navbar

--- src/Header.js
+++ src/Header.js
@@ -5,11 +5,12 @@ import { Link } from "react-router-dom";
 class Header extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar className="justify-content-between" collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
-        <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        <NavItem><Link to="/about" className="nav-link">About</Link></NavItem>
-
+        <div style={{display: "flex"}}>
+          <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
+          <NavItem><Link to="/about" className="nav-link">About</Link></NavItem>
+        </div>
       </Navbar>
     )
   }

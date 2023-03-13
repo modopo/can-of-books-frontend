12c8ef50af7b2d5ac837839b349ed8087f707b56
Jonathan Tsai <modopo@gmail.com>
2023-03-13 15:41:30 -0700

    added nav link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
+import About from './About';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/App.css'
 import {
   BrowserRouter as Router,
   Routes,
@@ -21,7 +23,13 @@ class App extends React.Component {
               element={<BestBooks />}
             >
             </Route>
-            {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
+            <Route
+              path="/about"
+              element={
+                <About />
+              }
+            >
+            </Route>
           </Routes>
           <Footer />
         </Router>

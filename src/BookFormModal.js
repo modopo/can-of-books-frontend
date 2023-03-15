af3a6ba517bf833f8ca314abc99e794e2aaf6c6d
Jonathan Tsai <modopo@gmail.com>
2023-03-15 16:35:39 -0700

    added update book feature with button on Carousel and by reusing bookFormModal for form input

--- src/BookFormModal.js
+++ src/BookFormModal.js
@@ -2,59 +2,91 @@ import React from 'react';
 import { Form, Button, Modal } from 'react-bootstrap';
 
 class BookFormModal extends React.Component {
-    handleClose= () => {
-        this.props.toggleModal();
+  handleClose = () => {
+    this.props.toggleModal();
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    let book;
+
+    if (!this.props.showUpdate) {
+      book = {
+        title: e.target.title.value,
+        description: e.target.description.value,
+        status: false,
+        image_url: e.target.imageUrl.value
+      };
+
+      this.props.addBook(book);
+    } else {
+      book = {
+        title: e.target.title.value || this.props.updatedBook.title,
+        description: e.target.description.value || this.props.updatedBook.description,
+        status: e.target.status.checked || this.props.updatedBook.status,
+        image_url: e.target.imageUrl.value || this.props.updatedBook.image_url
+      };
+
+      book._id = this.props.updatedBook._id;
+      book.__v = this.props.updatedBook.__v;
+
+      this.props.updateBook(book);
     }
 
-    handleSubmit= (e) => {
-        e.preventDefault();
-        let book = {
-            title: e.target.title.value,
-            description: e.target.description.value,
-            status: false,
-            image_url: e.target.imageUrl.value
-        };
-        this.handleClose();
-        this.props.addBook(book);
-    }
+    this.handleClose();
+  }
 
-    render() {
-        return(
-            <Modal show={this.props.showModal} onHide={this.handleClose}>
-                <Modal.Header>Add Book</Modal.Header>
-                <Modal.Body>
-                    <Form onSubmit={this.handleSubmit}>
-                        <Form.Group controlId="title">
-                            <Form.Label>
-                                Title
-                            </Form.Label>
-                            <Form.Control type="text">
+  render() {
+    let bookStatus = !this.props.showUpdate ? "Add Book" : "Update Book";
+    let updateExists = this.props.showUpdate;
 
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="description">
-                            <Form.Label>
-                                Description
-                            </Form.Label>
-                            <Form.Control type="text" controlId="description">
-                                
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="imageUrl">
-                            <Form.Label>
-                                Image URL
-                            </Form.Label>
-                            <Form.Control type="text" controlId="imageUrl">
-                                
-                            </Form.Control>
-                        </Form.Group>
-                        <Button type="submit">Add Book</Button>
-                        <Button onClick={this.handleClose}>Cancel</Button>
-                    </Form>
-                </Modal.Body>
-            </Modal>
-        );
-    }
+    return (
+      <Modal show={this.props.showModal} onHide={this.handleClose}>
+        <Modal.Header>{bookStatus}</Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group controlId="title" >
+              <Form.Label>
+                Title
+              </Form.Label>
+              <Form.Control type="text" placeholder={this.props.updatedBook.title}>
+
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="description" >
+              <Form.Label>
+                Description
+              </Form.Label>
+              <Form.Control type="text" controlId="description" placeholder={updateExists ? this.props.updatedBook.description : ''}>
+
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="imageUrl" >
+              <Form.Label>
+                Image URL
+              </Form.Label>
+              <Form.Control type="text" controlId="imageUrl" placeholder={updateExists ? this.props.updatedBook.image_url : ''}>
+
+              </Form.Control>
+            </Form.Group>
+
+            {this.props.showUpdate
+              &&
+              <Form.Group controlId="status">
+                <Form.Check type="checkbox" label="Checked Out">
+                </Form.Check>
+              </Form.Group>
+            }
+
+            <Button type="submit">{bookStatus}</Button>
+
+            <Button onClick={this.handleClose}>Cancel</Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    );
+  }
 }
 
 export default BookFormModal;
